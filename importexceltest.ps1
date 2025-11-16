cf8d528e8f3d9cfe1fd616006894c1323d07974a
shailangsa <49602578+shailangsa@users.noreply.github.com>
2025-11-16 19:42:24 +0000

    second commit

--- importexceltest.ps1
+++ importexceltest.ps1
@@ -11,23 +11,12 @@ foreach ($table in $worksheet.Tables) {
 		
 		Write-Host "Table Range Address: $($table.Range.Address)"
         
-        # Now you can work with the table, e.g., get the range or data
-        $tableData = $table.Range.Value
-        $tableData
-		$tableData | Format-Table -AutoSize | Out-Host
+ 
     }
 	
-	foreach ($row in $table.Range.Rows) {
-    # Skip the header row (first row)
-    if ($row.RowNumber -gt 1) {
-        $rowObj = @{}
-        for ($i = 1; $i -le $row.Cells.Count; $i++) {
-            $rowObj[$headers[$i - 1]] = $row.Cells[$i].Text
-        }
-        $tableData += New-Object PSObject -Property $rowObj
-    }
 	
-}# Output the data (if any)
+
+# Output the data (if any)
 if ($tableData.Count -gt 0) {
     $tableData | Format-Table -AutoSize | Out-Host
 } else {
